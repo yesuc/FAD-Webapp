3d3a1eb28fe639a5b042c70468445c237278bc8f
Priya Dhawka <pdhawka@colgate.edu>
2018-06-11 11:46:45 -0400

    Adding modified test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ A common ingredient in the Indian food is a wide range of spices. The secret of 
 Royal India Grill of Indian has prepared fresh items, paying careful attention to our patron’s choice of mind, medium or hot flavor. We do not use packaged curry powers or canned meat seafood or vegetables. Every dish at Royal India is prepared according to original which has become a part of our Indian culture over thousands of years. We are confident that your dining experience at Royal India will be a very pleasant one!
 Offering a Gluten Free Menu!'''
 
-rig = '''Shuruat/Appetizers
+item = '''Shuruat/Appetizers
 Veg Pakora
 3.95
 Fresh vegetable dipped in spices batter and fried to golden perfection.'''
@@ -31,28 +31,47 @@ Select Page'''
 test = [header, item, iguana]
 
 #Recomendation: try applying parser (below) to test examples array
+#NOTE: Removes all description text that appears before the first instance of 'Appetizers' and the last instance of 'Menu'
+# Does not negatively overwrite files which do not follow said output
+def process_text(filename):
+    with open(filename, 'r') as org_file:
+        original_text = org_file.read()
+    new_text = re.sub('Menu.*Appetizers\n', '', original_text, flags=re.DOTALL)
+    with open(filename, 'w') as new_file:
+        new_file.write(new_text)
+        
+#NOTE: Removes extra 'buzzwords such as time, date' and prices from file containing menu
+def iterate_menu(filename):
+    f= open(filename)
+    lines = f.readlines()
+    f.close()
+    f = open(filename,"w+")
+    for i in range(0, len(lines)):
+        lines[i] = lines[i].replace('$',' ')
+        lines[i] = ''.join(j for j in lines[i] if not j.isdigit())
+        lines[i] = re.sub('\s?(?:A.M|P.M|a.m|p.m)', ' ',lines[i])
+        lines[i] = re.sub('[a-z]?i+day', ' ',lines[i])
+        f.write(lines[i])
+    f.close()
 
-#Note: User should manually input path to Hamilton Royal Indian Grill Menu File to be read
-f = open('/Users/carteryesu/Desktop/Summer Project/webcrawler/menus-hamiltonroyalindiagrill.html','r')
+#Note: User should manually input path to Menu File to be read
+files = ['/Users/priyadhawka/Desktop/rigtester/menu-www.laiguanarestaurant.com.txt','/Users/priyadhawka/Desktop/rigtester/menu-www.noodles13.com.txt', '/Users/priyadhawka/Desktop/rigtester/menu-hamiltonroyalindiagrill.com.txt']
+filename = files[2]
+process_text(filename)
+iterate_menu(filename)
+f = open(filename,'r')
 text = f.read()
-grammar = r"""NP: {<JJ.?>*<NN.?>|<NNS.?>|<NN.?>|<NNPS.?>+ <VBN.*> <NN.*>.+}"""
-# grammar = r"""
-#     food: {<JJ.*>? <NN.*>+}
-#     verb: {<VBN.*>}
-#     """
-# grammar = """CHUNK: {<JJ>} """
-
-# Attempts to tag all words in given text, and also attempts to apply grammar parcer to filter only wanted phrases for items.
-for sent in text.splitlines():
-    tokens = 0
-    cp = nltk.RegexpParser(grammar)
-    result = cp.parse(nltk.pos_tag(nltk.word_tokenize(sent)))
-    print(result)
-    result.draw()
-    for word, pos in nltk.pos_tag(nltk.word_tokenize(sent)):
-
-        if (pos in ['NN','NNS','NNP','NNPS']):
-            tokens+=1
-    print("Nouns per Word in sentence: " + str(tokens/len(nltk.word_tokenize(sent))))
-    print('\n')
+grammar = r"""Chunk: {<JJ.?>* <NN.?>* <VB.?>* <NN.?>*}"""
+cp = nltk.RegexpParser(grammar)
+result = cp.parse(nltk.pos_tag(nltk.word_tokenize(text)))
+keys = []
+for t in result:
+    s = ''
+    if isinstance(t,nltk.tree.Tree):
+        for word in t:
+            if len(s) == 0:
+                s = word[0]
+            else:
+                s = s + ' ' + word[0]
+        keys.append(s)
 f.close()
