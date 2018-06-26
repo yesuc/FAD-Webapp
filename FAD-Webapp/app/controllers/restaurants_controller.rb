52d94801b6a5979203ec7dd4c40cfd8a2bfb0155
yesuc <ycarter@colgate.edu>
2018-06-26 11:33:30 -0400

    Added gems needed for testins-cucumber,rspec-added search route for restaurants'

--- FAD-Webapp/app/controllers/restaurants_controller.rb
+++ FAD-Webapp/app/controllers/restaurants_controller.rb
@@ -61,6 +61,10 @@ class RestaurantsController < ApplicationController
     end
   end
 
+ def search
+   
+ end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_restaurant
--- FAD-Webapp/config/routes.rb
+++ FAD-Webapp/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
+  resources :restaurants do
+    resources :food
+    get :search, :action => 'search'
+    # get "restaurants/search" => "restaurants#search", as: search_restaurants
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
