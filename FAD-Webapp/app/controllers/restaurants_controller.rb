d94cd8fb2a93f52ad4f3f245d27b880f4f67d28e
yesuc <ycarter@colgate.edu>
2018-06-28 14:23:53 -0400

    Restaurant Show Page Created, Creation Feature Almost done

--- FAD-Webapp/app/controllers/restaurants_controller.rb
+++ FAD-Webapp/app/controllers/restaurants_controller.rb
@@ -10,6 +10,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1
   # GET /restaurants/1.json
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   # GET /restaurants/new
@@ -19,6 +20,7 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants/1/edit
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   # POST /restaurants
@@ -26,7 +28,7 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(create_update_params)
     if @restaurant.save
-      redirect_to(root, :success => "Restaurant was successfully created.") and return
+      redirect_to(restaurant_path(@restaurant), :success => "Restaurant was successfully created.") and return
     else
       redirect_to(new_restaurant_path(@restaurant), :error => "Error creating new restaurant.") and return
     # respond_to do |format|
@@ -43,29 +45,31 @@ class RestaurantsController < ApplicationController
   # PATCH/PUT /restaurants/1
   # PATCH/PUT /restaurants/1.json
   def update
-    respond_to do |format|
-      if @restaurant.update(restaurant_params)
-        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully updated.' }
-        format.json { render :show, status: :ok, location: @restaurant }
-      else
-        format.html { render :edit }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
-      end
-    end
+    # respond_to do |format|
+    #   if @restaurant.update(restaurant_params)
+    #     format.html { redirect_to @restaurant, notice: 'Restaurant was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @restaurant }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /restaurants/1
   # DELETE /restaurants/1.json
   def destroy
     @restaurant.destroy
-    respond_to do |format|
-      format.html { redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    # respond_to do |format|
+    #   format.html { redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
   end
 
  def search
-   @q = params[:searchbar]
+   @q = "#{params[:query]}"
+   @restaurants = Restaurant.filter_on_constraints()
+   # @restaurants = Restaurant.where("name LIKE ? or url LIKE ? or address LIKE ? or cuisine LIKE ?", @q,@q,@q,@q).distinct
  end
 
 private
--- FAD-Webapp/app/models/restaurant.rb
+++ FAD-Webapp/app/models/restaurant.rb
@@ -1,4 +1,22 @@
 class Restaurant < ApplicationRecord
   has_many :menus
 
+  def self.filter_on_constraints(constraints)
+    filtered = Restaurant.all
+
+    constraints.each_pair do |sym,val|
+      if sym == :name
+        filtered = filtered.where("name LIKE ?", val)
+      elsif sym == :url
+        filtered = filtered.where("url LIKE ?", val)
+      elsif sym == :address
+        filtered = filtered.where("address LIKE ?", val)
+      elsif sym == :cuisine
+        filtered = filtered.where("cuisine LIKE ?", val)
+      end
+    end
+    return filtered
+  end
+
+
 end
